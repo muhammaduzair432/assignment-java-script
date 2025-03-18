f4e4e92a9d32cb3a5da13ccc80c5b52461055e44
muhammad uzair <abuzada735@gmail.com>
2025-03-18 06:55:12 +0500

    assignment dne
    assignment done

---
+++ index.js
@@ -0,0 +1,37 @@
+// assignment 1
+//  random password generator
+// input=prompt("enter a password");
+// const password=("");
+// const alphabets="abcdef12345@$!&";
+// for(let i=1; i<=8;i++){
+//     let password =(Math.random()*alphabets.length+1);
+
+// }
+//  console.log(password);
+
+// task2
+// let input=prompt("enter a word than can be read forward as same backward")
+// let str =(""+input).split("");
+// let n =str.length;
+// for(let i=0; i<n; i++){
+//     if (str[i] != str[n-1-i]){
+//         console.log("not a palindrome");
+//     }
+//     else{
+//         console.log("palindrome")
+//     }
+// }
+
+// task3
+// input= prompt("enter a word");
+// const tree= (""+input).split('')
+// let size=tree.length;
+// console.log(size)
+
+// let date =new Date();
+// let year= Date.getFullYear();
+// console.log(year)
+// let month=Date.getmonth();
+// let day=Date.getday();
+// let date=(day+'/'+month+'/'+year);
+// console.log(year);
